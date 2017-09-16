9c57d4c9636cc6819e1a873853716d67a48c5e81
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-09-15 22:16:42 -1000

    implemented revealing module pattern, fixed indentation

--- quickSort.js
+++ quickSort.js
@@ -1,25 +1,33 @@
-function quickSort(arr){
+var quickSort = function() {
 
-  var lessArray = [];
-  var greaterArr = [];
-  var pivot = [arr[0]];
 
-  if (arr.length === 0){
-    return arr;
-  }
+  function sortArray(arr){
 
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] < pivot){
-      lessArray.push(arr[i]);
-    }else{
-      greaterArr.push(arr[i]);
+    var lessArray = [];
+    var greaterArr = [];
+    var pivot = [arr[0]];
+
+    if (arr.length === 0){
+      return arr;
     }
+
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot){
+        lessArray.push(arr[i]);
+      }else{
+        greaterArr.push(arr[i]);
+      }
+    }
+    return sortArray(lessArray).concat(pivot.concat(sortArray(greaterArr)));
   }
 
-  return quickSort(lessArray).concat(pivot.concat(quickSort(greaterArr)));
-}
+  return{
+    sortArray : sortArray,
+  };
+};
 
 
 
 var array = [5, 9, 1, 10, 10, 4, 14, 7, 3, 4];
-console.log(quickSort(array));
+
+console.log(quickSort().sortArray(array));
