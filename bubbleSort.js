35f0c59cba9f1cc6335f2a36886201db91192751
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-12-25 13:28:35 -0800

    rework code, add notes about time complexity

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,17 +1,21 @@
-var unsorted = [5, 6, 1, 8, 2, 4, 9, 3];
+//Repeatedly iterates through the array, compares each pair of adjacent items and swaps them if they're in the wrong order, until they're all in the right order.
+//It's simple but slow. Avoid for large lists.
+//Possibly useful if the list is mostly sorted, but has some barely out-of-order elements.
 
-function bubbleSort(array){
-  for (var j = 0; j < array.length - 1; j++){
-    for (var i = 0; i < array.length - 1; i++) {
-      if(array[i] > array[i + 1]){
-        var temp = null;
-        temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
+//Time complexity: ((best & worst case))
+//O(n^2) [[n is number of items to be sorted]]
+
+function bubbleSort(arr){
+  for (var i = 0; i < arr.length - 1; i++){
+    for(var j = 0; j < arr.length - 1; j++){
+      if (arr[j] > arr[j + 1]){
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
-  return array;
+  return arr;
 }
 
-var sorted = bubbleSort(unsorted);
+console.log(bubbleSort([9,7,5,4,3,1]));
