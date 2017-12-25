763dfcd65e1147e43003774ad2537824d1800d7f
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-12-25 13:36:50 -0800

    fix var syntax

--- bubbleSort.js
+++ bubbleSort.js
@@ -6,8 +6,8 @@
 //O(n^2) [[n is number of items to be sorted]]
 
 function bubbleSort(arr){
-  for (var i = 0; i < arr.length - 1; i++){
-    for(var j = 0; j < arr.length - 1; j++){
+  for (let i = 0; i < arr.length - 1; i++){
+    for(let j = 0; j < arr.length - 1; j++){
       if (arr[j] > arr[j + 1]){
         let temp = arr[j];
         arr[j] = arr[j + 1];
