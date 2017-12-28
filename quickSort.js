89243d581ef9cf5ab0f73be83ce990f35feceb07
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-12-27 20:20:52 -0800

    notes and syntax update

--- quickSort.js
+++ quickSort.js
@@ -1,33 +1,33 @@
-var quickSort = function() {
+//Divide an array into two sub-arrays: smaller, and larger.
+//Picks a pivot, orders elements so lesser come before the pivot, and greater go after the pivot. Then recursively do the same for the lesser sub-array and the greater sub-array. Repeatrepeatrepeat.
 
+//I used arr[0] as the pivot, which is BAD news for already sorted arrays. You really should choose a random index for the pivot, or median, instead.
 
-  function sortArray(arr){
+//Much more efficient than bubble & selection. Can be 2~3 times faster than merge sort & heapsort.
 
-    var lessArray = [];
-    var greaterArr = [];
-    var pivot = [arr[0]];
+//Average time complexity: O(n log(n))
+//Worse case: O(n^2), but this is rare.
 
-    if (arr.length === 0){
-      return arr;
-    }
+function quickSort(arr){
 
-    for (var i = 1; i < arr.length; i++) {
-      if (arr[i] < pivot){
-        lessArray.push(arr[i]);
-      }else{
-        greaterArr.push(arr[i]);
-      }
-    }
-    return sortArray(lessArray).concat(pivot.concat(sortArray(greaterArr)));
+  let smaller = [];
+  let larger = [];
+  let centerIndex = Math.round((arr.length - 1)/2);
+  let pivot = [arr[0]];
+
+  if (arr.length === 0){
+    return arr;
   }
 
-  return{
-    sortArray : sortArray,
-  };
+  for (let i = 1; i < arr.length; i++){
+    if (arr[i] < pivot){
+      smaller.push(arr[i]);
+    }else{
+      larger.push(arr[i]);
+    }
+  }
+
+  return quickSort(smaller).concat(pivot.concat(quickSort(larger)));
 };
 
-
-
-var array = [5, 9, 1, 10, 10, 4, 14, 7, 3, 4];
-
-quickSort().sortArray(array);
+let array = [5, 9, 1, 10, 10, 4, 14, 7, 3, 4];
