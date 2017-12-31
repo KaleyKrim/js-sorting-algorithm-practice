ab2ce3ef4b76a5364eb34ef5c5d98c0438a77859
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-12-31 10:42:20 -0800

    add notes

--- mergeSort.js
+++ mergeSort.js
@@ -10,6 +10,8 @@ function mergeSort (arr) {
     return arr;
   }
 
+  //recursively slice the array down the middle into a left and right side, until the length of each array is 1.
+
   const middle = Math.floor(arr.length/2);
   const left = arr.slice(0, middle);
   const right = arr.slice(middle);
@@ -25,16 +27,20 @@ function merge (left, right) {
   let indexLeft = 0
   let indexRight = 0
 
+  //so, while we're still iterating thru the arrays, and haven't reached the end yet.
   while (indexLeft < left.length && indexRight < right.length) {
+    //if the value in the left array is smaller than the one in the right array, push that left value into results, and increment the left index, so we can check the next value next time.
     if (left[indexLeft] < right[indexRight]) {
       result.push(left[indexLeft]);
       indexLeft++;
     } else {
+      //otherwise, push the right value into results, and increment right index.
       result.push(right[indexRight]);
       indexRight++;
     }
   }
 
+  //concat the sorted results array with the rest of the left array & right array.
   return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
 }
 
